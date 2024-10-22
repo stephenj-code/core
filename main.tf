d0f67ac9e153b41e6937c75f58cc4f46f8fd5a14
log-gt <160951553+log-gt@users.noreply.github.com>
2024-10-21 22:57:06 -0400

    corrected ami for us-east-2

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "chicken-wing" {
-  ami           = "ami-06b21ccaeff8cd686" # Replace with the actual AMI ID for your region
+  ami           = "ami-050cd642fd83388e4" # Replace with the actual AMI ID for your region
   instance_type = "t2.micro"
 
   tags = {
