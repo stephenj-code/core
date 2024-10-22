fdef0c135ff626796424844a1f4623c8f85ed6f5
stephen jayaraj <160951553+stephenj-code@users.noreply.github.com>
2024-10-21 23:32:08 -0400

    Update main.tf with vpc  and subnet

--- main.tf
+++ main.tf
@@ -3,12 +3,30 @@ provider "aws" {
     profile = "default"
 }
 
-resource "aws_instance" "chicken-wing" {
-  ami           = "ami-050cd642fd83388e4" # Replace with the actual AMI ID for your region
-  instance_type = "t2.micro"
+resource "aws_vpc" "main-vpc" {
+  cidr_block = "20.24.0.0/16"
 
   tags = {
-    Name = "MyTerraformInstance"
+    Name = "main-vpc"
   }
 }
 
+resource "aws_subnet" "public-subnet" {
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = "20.24.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_instance" "inst-01" {
+  ami           = "ami-050cd642fd83388e4" # Replace with the actual AMI ID for your region
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main-subnet.id
+
+  tags = {
+    Name = "inst-01"
+  }
+}
