6baf175b0801a175d45bd12eebe1d35fadd19891
log-gt <160951553+log-gt@users.noreply.github.com>
2024-10-22 00:43:52 -0400

    added Createdby tag

--- main.tf
+++ main.tf
@@ -8,28 +8,31 @@ resource "aws_vpc" "main-vpc" {
   tags = {
     Name       = "main-vpc"
     CreateDate = local.current_date
+    CreatedBy  = "Terraform"
   }
 }
 
 resource "aws_subnet" "private-subnet" {
-  vpc_id            = aws_vpc.main-vpc.id
-  cidr_block        = "20.24.0.0/24"
+  vpc_id                  = aws_vpc.main-vpc.id
+  cidr_block              = "20.24.0.0/24"
   map_public_ip_on_launch = false
 
   tags = {
-    Name        = "private-subnet"
-    CreateDate  = local.current_date
+    Name       = "private-subnet"
+    CreateDate = local.current_date
+    CreatedBy  = "Terraform"
   }
 }
 
 resource "aws_subnet" "public-subnet" {
-  vpc_id            = aws_vpc.main-vpc.id
-  cidr_block        = "20.24.1.0/24"
+  vpc_id                  = aws_vpc.main-vpc.id
+  cidr_block              = "20.24.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name        = "public-subnet"
-    CreateDate  = local.current_date
+    Name       = "public-subnet"
+    CreateDate = local.current_date
+    CreatedBy  = "Terraform"
   }
 }
 
@@ -39,8 +42,9 @@ resource "aws_instance" "public-instance-01" {
   subnet_id     = aws_subnet.public-subnet.id
 
   tags = {
-    Name        = "public-instance-01"
-    CreateDate  = local.current_date
+    Name       = "public-instance-01"
+    CreateDate = local.current_date
+    CreatedBy  = "Terraform"
   }
 }
 
@@ -50,7 +54,8 @@ resource "aws_instance" "private-instance-01" {
   subnet_id     = aws_subnet.private-subnet.id
 
   tags = {
-    Name        = "private-instance-01"
-    CreateDate  = local.current_date
+    Name       = "private-instance-01"
+    CreateDate = local.current_date
+    CreatedBy  = "Terraform"
   }
 }
