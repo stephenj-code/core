32a3a50b77f9354c3784be5bf5ed0e531e097153
log-gt <160951553+log-gt@users.noreply.github.com>
2024-10-22 01:53:50 -0400

    tags variable

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "inst_type" {
   description = "The type of instance to use"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "tags" {
@@ -9,5 +8,6 @@ variable "tags" {
   type        = map(string)
   default = {
     CreatedBy  = "Terraform"
+    Identifier = "main-vpc"
   }
 }
