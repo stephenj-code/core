1f10f500174205ccceba91863001a7b9ef1a1ba1
log-gt <160951553+log-gt@users.noreply.github.com>
2024-10-22 00:01:57 -0400

    CreateDate tag is added and some cleanup done

--- main.tf
+++ main.tf
@@ -1,13 +1,24 @@
-provider "aws" {
-    region = "us-east-2"
-    profile = "default"
+locals {
+  current_date = formatdate("YYYY-MM-DD", timestamp())
 }
 
 resource "aws_vpc" "main-vpc" {
   cidr_block = "20.24.0.0/16"
 
   tags = {
-    Name = "main-vpc"
+    Name       = "main-vpc"
+    CreateDate = local.current_date
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = "20.24.0.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "private-subnet"
+    CreateDate  = local.current_date
   }
 }
 
@@ -17,16 +28,29 @@ resource "aws_subnet" "public-subnet" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet"
+    Name        = "public-subnet"
+    CreateDate  = local.current_date
   }
 }
 
-resource "aws_instance" "inst-00" {
+resource "aws_instance" "public-instance-01" {
   ami           = "ami-050cd642fd83388e4" # Replace with the actual AMI ID for your region
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public-subnet.id
 
   tags = {
-    Name = "inst-00"
+    Name        = "public-instance-01"
+    CreateDate  = local.current_date
+  }
+}
+
+resource "aws_instance" "private-instance-01" {
+  ami           = "ami-050cd642fd83388e4" # Replace with the actual AMI ID for your region
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private-subnet.id
+
+  tags = {
+    Name        = "private-instance-01"
+    CreateDate  = local.current_date
   }
 }
