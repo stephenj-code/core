82386039c59688b2f1440f4412b0072b31f1f961
log-gt <160951553+log-gt@users.noreply.github.com>
2024-10-22 23:50:01 -0400

    vars.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_subnet" "public-subnet" {
 
 resource "aws_instance" "public-instance-01" {
   ami           = "ami-050cd642fd83388e4" # Replace with the actual AMI ID for your region
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   subnet_id     = aws_subnet.public-subnet.id
   key_name      = "bastion-ssh"
 
@@ -50,7 +50,7 @@ resource "aws_instance" "public-instance-01" {
 
 resource "aws_instance" "private-instance-01" {
   ami           = "ami-050cd642fd83388e4" # Replace with the actual AMI ID for your region
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   subnet_id     = aws_subnet.private-subnet.id
   key_name      = "bastion-ssh"
 
---
+++ vars.tf
@@ -0,0 +1,5 @@
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+  default     = "t2.micro"
+}
